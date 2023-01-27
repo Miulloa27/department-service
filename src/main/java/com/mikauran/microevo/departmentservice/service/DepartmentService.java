7f8bed994020addbe26ea648755d8cf5d8d4dc34
Miguel Lopez <miguel.lopez@nagarro.com>
2023-01-27 17:00:17 -0600

    Implementing the first Microservice Department with some other important items

---
+++ src/main/java/com/mikauran/microevo/departmentservice/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.mikauran.microevo.departmentservice.service;
+
+import com.mikauran.microevo.departmentservice.entity.Department;
+import com.mikauran.microevo.departmentservice.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
